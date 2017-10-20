c2d86ba88f5aef1116534f224d5494a7525c631d
Matt182 <mattspring0@gmail.com>
2017-10-20 19:24:52 +0000

    generator

---
+++ user_profile/management/commands/generator.py
@@ -0,0 +1,37 @@
+from django.core.management import BaseCommand
+from faker import Faker
+
+from user_profile.models import User
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--delete',
+            action='store_true',
+            default=False,
+            help='Delete users',
+        )
+
+        parser.add_argument(
+            '--generate',
+            action='store_true',
+            default=False,
+            help='Generates users',
+        )
+
+    def handle(self, *args, **options):
+        if options['delete']:
+            clear()
+        if options['generate']:
+            generate()
+
+
+def generate():
+    faker = Faker()
+    for i in range(5):
+        User.objects.create(id=i, name=faker.name(), email=faker.email(), password=faker.first_name())
+
+
+def clear():
+    User.objects.all().delete()
