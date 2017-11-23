e307b6049f0d736ac09465f7797fdaa78066c831
Matt182 <mattspring0@gmail.com>
2017-11-23 20:14:04 +0000

    bootstrap

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,10 @@
 import path from 'path'
 import webpack from 'webpack'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+
+const extractSass = new ExtractTextPlugin({
+  filename: "styles.css",
+});
 
 export default {
    entry:  './static_source/app.js',
@@ -11,14 +16,36 @@ export default {
        rules: [
            {
                test: /\.js$/,
-               use: 'babel-loader'
+               use: {
+                   loader: 'babel-loader',
+                   options: {
+                       ignore: './node_modules/',
+                       presets: [
+                           ['es2015', { modules: false }]
+                       ]
+                   }
+               }
            },
            {
                test: /\.s[a|c]ss$/,
-               use: [
-                   'style-loader!css-loader!sass-loader'
-               ]
+               use: extractSass.extract({
+                   fallback: 'style-loader',
+                   //resolve-url-loader may be chained before sass-loader if necessary
+                   use: [{
+                       loader: "css-loader" // translates CSS into CommonJS
+                   }, {
+                       loader: "sass-loader" // compiles Sass to CSS
+                   }]
+               })
            }
        ]
    },
+   plugins: [
+       new webpack.ProvidePlugin({
+           $: "jquery", // Used for Bootstrap JavaScript components
+           jQuery: "jquery", // Used for Bootstrap JavaScript components
+           Popper: ['popper.js', 'default'] // Used for Bootstrap dropdown, popup and tooltip JavaScript components
+       }),
+       extractSass
+   ]
 }
